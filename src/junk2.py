961321ada56cefdccd444e6062ee7c18a4b49071
Jon Schuermann <schuerjp@anl.gov>
2016-03-01 13:55:15 -0600

    updated files to test sercat

--- src/junk2.py
+++ src/junk2.py
@@ -2,8 +2,15 @@ import rapd_utils as Utils
 import rapd_beamlinespecific as BLspec
 #Utils.readMarHeader('/gpfs6/users/necat/Jon/Programs/CCTBX_x64/modules/dials_regression/image_examples/APS_22ID/junk_r1_1.0001')
 from multiprocessing import Process, Queue
-os.chdir('/home/schuerjp/temp')
-queue = Queue.Queue()
-job = Process(target=BLspec.processCluster,args=(self,'aimless',queue))
-job.start()
-print queue.get()
+
+class Junk(Process):
+  def __init__(self):
+    Process.__init__(self,)
+    self.start()
+  
+  def run(self):
+    os.chdir('/home/schuerjp/temp')
+    queue = Queue.Queue()
+    job = Process(target=BLspec.processCluster,args=(self,'aimless',queue))
+    job.start()
+    print queue.get()
