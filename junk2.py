67b183397503516771872faef632ebbdffac050d
frankvm04 <frankvmurphy@gmail.com>
2016-03-14 12:15:12 -0500

    fixed run indentation and bath to bm images

--- junk2.py
+++ junk2.py
@@ -1,8 +1,9 @@
 import os
-import rapd_utils as Utils
-import rapd_beamlinespecific as BLspec
+#import rapd_utils as Utils
+#import rapd_beamlinespecific as BLspec
 #Utils.readMarHeader('/gpfs6/users/necat/Jon/Programs/CCTBX_x64/modules/dials_regression/image_examples/APS_22ID/junk_r1_1.0001')
 from multiprocessing import Process, Queue
+from src.sites.cluster.sercat import process_cluster
 
 class Junk(Process):
   def __init__(self):
@@ -12,8 +13,10 @@ class Junk(Process):
   def run(self):
     os.chdir('/home/schuerjp/temp')
     queue = Queue()
-    command = '/home/schuerjp/Programs/RAPD/bin/rapd.python /home/schuerjp/Programs/RAPD/src/agents/rapd_agent_strategy.py'
-    job = Process(target=BLspec.processClusterSercat,args=(self,(command,'labelit.log'),queue))
+    #command = '/home/schuerjp/Programs/RAPD/bin/rapd.python /home/schuerjp/Programs/RAPD/src/agents/rapd_agent_strategy.py'
+    command = 'touch junk1'
+    #job = Process(target=BLspec.processClusterSercat,args=(self,(command,'labelit.log'),queue))
+    job = Process(target=process_cluster,args=(self,(command,'labelit.log'),queue))
     job.start()
     print queue.get()
     
