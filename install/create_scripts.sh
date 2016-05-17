7b891f02abf4b10dbb9341fd64cc4a8e0a30e1f8
Frank Murphy <fvm3@cornell.edu>
2016-05-17 14:13:18 -0500

    almost

--- install/create_scripts.sh
+++ install/create_scripts.sh
@@ -7,23 +7,23 @@ if [ "$RAPD_HOME" != "" ]; then
   # Create scripts
   echo "#! /bin/bash" > ./rapd.control
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py --managed_file $SAFE_PREFIX\/src\/control\/rapd_control.py \"\$@\"" >>$SAFE_PREFIX\/bin\/rapd.control
-  chmod +x ./rapd.control
+  chmod +x $SAFE_PREFIX\/bin\/rapd.control
 
   echo "#! /bin/bash" > ./rapd.gather
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py --managed_file $SAFE_PREFIX\/src\/sites\/gather\/rapd_gather.py \"\$@\"" >>$SAFE_PREFIX\/bin\/rapd.gather
-  chmod +x ./rapd.gather
+  chmod +x $SAFE_PREFIX\/bin\/rapd.gather
 
   echo "#! /bin/bash" > ./rapd.launcher
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py --managed_file $SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/launch\/rapd_launcher.py \"\$@\"" >>$SAFE_PREFIX\/bin\/rapd.launcher
-  chmod +x ./rapd.launcher
+  chmod +x $SAFE_PREFIX\/bin\/rapd.launcher
 
   echo "#! /bin/bash" > ./rapd.launch
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/launch\/rapd_launch.py \"\$@\"" >>$SAFE_PREFIX\/bin\/rapd.launch
-  chmod +x ./rapd.launch
+  chmod +x $SAFE_PREFIX\/bin\/rapd.launch
 
   echo "#! /bin/bash" > ./rapd.overwatch
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py \"\$@\"" >>$SAFE_PREFIX\/bin\/rapd.overwatch
-  chmod +x ./rapd.overwatch
+  chmod +x $SAFE_PREFIX\/bin\/rapd.overwatch
 
 # Environmental var not set - don't run
 else
