a88a102212953d8cb33fab77022ae3a947b99ca4
Frank Murphy <fvm3@cornell.edu>
2016-05-18 10:12:15 -0500

    Error in bin script creation...

--- install/create_scripts.sh
+++ install/create_scripts.sh
@@ -5,23 +5,23 @@ if [ "$RAPD_HOME" != "" ]; then
   SAFE_PREFIX=$(echo "$RAPD_HOME" | sed -e 's/\//\\\//g')
 
   # Create scripts
-  echo "#! /bin/bash" > ./rapd.control
+  echo "#! /bin/bash" > $RAPD_HOME/bin/rapd.control
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py --managed_file $SAFE_PREFIX\/src\/control\/rapd_control.py \"\$@\"" >>$RAPD_HOME/bin/rapd.control
   chmod +x $RAPD_HOME/bin/rapd.control
 
-  echo "#! /bin/bash" > ./rapd.gather
+  echo "#! /bin/bash" > $RAPD_HOME/bin/rapd.gather
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py --managed_file $SAFE_PREFIX\/src\/gather\/rapd_gather.py \"\$@\"" >>$RAPD_HOME/bin/rapd.gather
   chmod +x $RAPD_HOME/bin/rapd.gather
 
-  echo "#! /bin/bash" > ./rapd.launcher
+  echo "#! /bin/bash" > $RAPD_HOME/bin/rapd.launcher
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py --managed_file $SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/launch\/rapd_launcher.py \"\$@\"" >>$RAPD_HOME/bin/rapd.launcher
   chmod +x $RAPD_HOME/bin/rapd.launcher
 
-  echo "#! /bin/bash" > ./rapd.launch
+  echo "#! /bin/bash" > $RAPD_HOME/bin/rapd.launch
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/launch\/rapd_launch.py \"\$@\"" >>$RAPD_HOME/bin/rapd.launch
   chmod +x $RAPD_HOME/bin/rapd.launch
 
-  echo "#! /bin/bash" > ./rapd.overwatch
+  echo "#! /bin/bash" > $RAPD_HOME/bin/rapd.overwatch
   echo "$SAFE_PREFIX\/bin\/rapd.python $SAFE_PREFIX\/src\/utils\/overwatch.py \"\$@\"" >>$RAPD_HOME/bin/rapd.overwatch
   chmod +x $RAPD_HOME/bin/rapd.overwatch
 
