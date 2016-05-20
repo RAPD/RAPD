cc66e350244c086b02536c7288a4f3a1b3e3c6c0
Frank Murphy <fvm3@cornell.edu>
2016-05-20 15:33:00 -0500

    POE

--- src/agents/rapd_agent_integrate.py
+++ src/agents/rapd_agent_integrate.py
@@ -53,9 +53,9 @@ from numpy import interp
 
 # RAPD imports
 from subcontractors.xdsme.xds2mos import Xds2Mosflm
-from utils.communicate import rapd_send
-from rapd_agent_stats import AutoStats
-import utils.xutils as Utils
+# from utils.communicate import rapd_send
+# from rapd_agent_stats import AutoStats
+# import utils.xutils as Utils
 #
 # # Import smartie.py from the installed CCP4 package
 # # smartie.py is a python script for parsing log files from CCP4
