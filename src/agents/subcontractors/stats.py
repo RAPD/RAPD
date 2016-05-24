2f288cd0f1a253734747f26819e2cc33d88fb8d4
Frank Murphy <fvm3@cornell.edu>
2016-05-24 14:38:09 -0500

    Working on getting the precession photos handled and linted

--- src/agents/subcontractors/stats.py
+++ src/agents/subcontractors/stats.py
@@ -31,8 +31,8 @@ import os
 import time
 
 # RAPD imports
-# from agents.rapd_agent_cell import PDBQuery
-# from agents.rapd_agent_pp import LabelitPP
+from cell import PDBQuery
+from precession import LabelitPP
 import parse as Parse
 import summary as Summary
 from utils.communicate import rapd_send
