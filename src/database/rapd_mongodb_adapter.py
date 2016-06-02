f363aefff59f817aaebf7e93025314790f694ca6
Frank Murphy <fvm3@cornell.edu>
2016-06-02 10:06:57 -0500

    Was updating the wrong collection!

--- src/database/rapd_mongodb_adapter.py
+++ src/database/rapd_mongodb_adapter.py
@@ -1023,9 +1023,9 @@ class Database(object):
         agent_result["timestamp"] = datetime.datetime.utcnow()
 
         # Add to results
-        result = db.agent_processes.update({"process.agent_process_id":agent_result["process"]["agent_process_id"]},
-                                           {"$set":agent_result},
-                                           upsert=True)
+        result = db.agent_results.update({"process.agent_process_id":agent_result["process"]["agent_process_id"]},
+                                         {"$set":agent_result},
+                                         upsert=True)
 
         self.logger.debug(result)
 
