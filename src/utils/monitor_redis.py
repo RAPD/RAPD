6329b5ba32b433302f6eefc341ff4da51009772a
Frank Murphy <fvm3@cornell.edu>
2017-02-14 10:27:17 -0600

    Updating tool to have better style

--- src/utils/monitor_redis.py
+++ src/utils/monitor_redis.py
@@ -1,12 +1,58 @@
 """
 Monitor script for redis.
 
-Taken from http://stackoverflow.com/questions/10458146/how-can-i-mimic-the-redis-monitor-command-in-a-python-script-using-redis-py
+Idea from http://stackoverflow.com/questions/10458146/how-can-i-mimic-the-redis-monitor-command-in-a-python-script-using-redis-py
 """
 
+"""
+This file is part of RAPD
+
+Copyright (C) 2017, Cornell University
+All rights reserved.
+
+RAPD is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3.
+
+RAPD is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__created__ = "2017-02-14"
+_maintainer__ = "Your name"
+__email__ = "Your email"
+__status__ = "Development"
+
+# Standard imports
+import argparse
+# import datetime
+# import glob
+# import json
+# import logging
+# import multiprocessing
+# import os
+# import pprint
+# import pymongo
+# import re
 import redis
+# import shutil
+# import subprocess
+# import sys
+# import time
+# import unittest
+
+# RAPD imports
+# import commandline_utils
+# import detectors.detector_utils as detector_utils
+# import utils
 
 class Monitor():
+    """Run monitor on a redis instance"""
     def __init__(self, connection_pool):
         self.connection_pool = connection_pool
         self.connection = None
@@ -24,8 +70,7 @@ class Monitor():
 
     def monitor(self):
         if self.connection is None:
-            self.connection = self.connection_pool.get_connection(
-                "monitor", None)
+            self.connection = self.connection_pool.get_connection("monitor", None)
         self.connection.send_command("monitor")
         return self.listen()
 
@@ -36,10 +81,54 @@ class Monitor():
         while True:
             yield self.parse_response()
 
-if  __name__ == '__main__':
-    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+def main(args):
+    """
+    Coordinate monitoring from the commandline
+    """
+
+    pool = redis.ConnectionPool(host=args.host, port=args.port, db=args.db)
     monitor = Monitor(pool)
     commands = monitor.monitor()
 
     for c in commands :
         print(c)
+
+def get_commandline():
+    """
+    Grabs the commandline
+    """
+
+    # Parse the commandline arguments
+    commandline_description = "Monitor redis instrance activity"
+    parser = argparse.ArgumentParser(description=commandline_description)
+
+    # Host
+    parser.add_argument("-o", "--host",
+                        action="store",
+                        dest="host",
+                        default="localhost",
+                        help="Host of redis to be monitored")
+
+    # Port
+    parser.add_argument("-p", "--port",
+                        action="store",
+                        dest="port",
+                        default=6379,
+                        type=int,
+                        help="Port of redis to be monitored")
+
+    # Database
+    parser.add_argument("-d", "--db",
+                        action="store",
+                        dest="db",
+                        default=0,
+                        type=int,
+                        help="DB to be monitored")
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    commandline_args = get_commandline()
+
+    main(args=commandline_args)
