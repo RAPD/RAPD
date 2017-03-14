00892aca174507175596cbce60d74321da52c94a
Frank Murphy <fmurphy@Franks-MacBook-Pro.local>
2017-03-14 17:00:11 -0500

    Debugging

--- src/plugins/integrate/plugin.py
+++ src/plugins/integrate/plugin.py
@@ -635,7 +635,6 @@ class RapdPlugin(Process):
         # If low resolution, don't try to polish the data, as this tends to blow up.
         if new_rescut <= 4.5:
             pprint(newinp)
-            sys.exit()
             os.rename('%s/GXPARM.XDS' % xdsdir, '%s/XPARM.XDS' % xdsdir)
             os.rename('%s/CORRECT.LP' % xdsdir, '%s/CORRECT.LP.old' % xdsdir)
             os.rename('%s/XDS.LOG' % xdsdir, '%s/XDS.LOG.old' % xdsdir)
