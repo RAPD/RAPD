a0982afada082697be53d3fe9fdb980083930d0a
Frank Murphy <fvm3@cornell.edu>
2017-08-18 13:33:05 -0500

    Dummy tprint not doing it...

--- src/plugins/integrate/plugin.py
+++ src/plugins/integrate/plugin.py
@@ -152,7 +152,8 @@ class RapdPlugin(Process):
             self.tprint = tprint
         # Dead end if no tprint passed
         else:
-            def func(*arg):
+            def func(*args, **kwargs):
+                """Dummy function"""
                 pass
             self.tprint = func
 
