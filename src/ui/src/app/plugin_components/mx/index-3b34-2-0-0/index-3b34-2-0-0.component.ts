c53a211bbd2eb4c8b8cf69690cd1f96f8c2a55b0
Frank Murphy <frankvmurphy@gmail.com>
2017-08-28 11:30:43 -0500

    Getting ready to add the image header information card

--- src/ui/src/app/plugin_components/mx/index-3b34-2-0-0/index-3b34-2-0-0.component.ts
+++ src/ui/src/app/plugin_components/mx/index-3b34-2-0-0/index-3b34-2-0-0.component.ts
@@ -1,9 +1,12 @@
 import { Component,
          Input,
          OnDestroy,
-         OnInit } from '@angular/core';
+         OnInit,
+         Pipe,
+         PipeTransform } from '@angular/core';
 import { MdDialog,
-         MD_DIALOG_DATA } from '@angular/material';
+         MD_DIALOG_DATA,
+         MdSnackBar } from '@angular/material';
 import { ReplaySubject }   from 'rxjs/Rx';
 import { ResultsService } from '../../../shared/services/results.service';
 import { PlotOmegaStartComponent } from './plot-omega-start/plot-omega-start.component'
@@ -17,6 +20,7 @@ export class Index3b34200Component implements OnInit {
 
   @Input() current_result: any;
   full_result: any;
+  index_result: any;
 
   incomingData$: ReplaySubject<string>;
 
@@ -38,6 +42,18 @@ export class Index3b34200Component implements OnInit {
   public handleIncomingData(data: any) {
     console.log('handleIncomingData', data);
     this.full_result = data;
+
+    this.index_result = [{
+      spacegroup: this.full_result.results.labelit_results.best_sym,
+      unit_cell: this.full_result.results.labelit_results.best_cell,
+      mosaicity: this.full_result.results.labelit_results.mosflm_mos[0],
+      resolution: this.full_result.results.labelit_results.mosflm_res[0],
+    },];
+  }
+
+  // Display the header information
+  displayHeader(header) {
+    console.log(header);
   }
 
   plotOmegaStart(mode) {
@@ -212,3 +228,24 @@ export class Index3b34200Component implements OnInit {
     let dialogRef = this.dialog.open(PlotOmegaStartComponent, config);
   }
 }
+
+/*
+ * Raise the value exponentially
+ * Takes an exponent argument that defaults to 1.
+ * Usage:
+ *   value | exponentialStrength:exponent
+ * Example:
+ *   {{ 2 | exponentialStrength:10 }}
+ *   formats to: 1024
+*/
+// @Pipe({name: 'fullToIndexResult'})
+// export class FullToIndexResultPipe implements PipeTransform {
+//   transform(full_result: any): any {
+//     return [{
+//       spacegroup: full_result.results.labelit_results.best_sym,
+//       unit_cell: full_result.results.labelit_results.best_cell,
+//       mosaicity: full_result.results.labelit_results.mosflm_mos[0],
+//       resolution: full_result.results.labelit_results.mosflm_res[0]
+//     }];
+//   }
+// }
