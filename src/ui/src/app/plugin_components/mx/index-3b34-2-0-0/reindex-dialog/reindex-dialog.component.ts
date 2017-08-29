e1dbfa127088fc4e90ef4e8fa393a94862930de4
Frank Murphy <frankvmurphy@gmail.com>
2017-08-28 21:29:02 -0500

    Working on reindex dialog

--- src/ui/src/app/plugin_components/mx/index-3b34-2-0-0/reindex-dialog/reindex-dialog.component.ts
+++ src/ui/src/app/plugin_components/mx/index-3b34-2-0-0/reindex-dialog/reindex-dialog.component.ts
@@ -5,6 +5,7 @@ import { FormGroup,
          FormControl } from '@angular/forms';
 import { MdDialogRef,
          MD_DIALOG_DATA } from '@angular/material';
+
 import { GlobalsService } from '../../../../shared/services/globals.service';
 
 @Component({
@@ -14,12 +15,42 @@ import { GlobalsService } from '../../../../shared/services/globals.service';
 })
 export class ReindexDialogComponent implements OnInit {
 
+  submitted: boolean = false;
+  model: any;
+  reindex_form: FormGroup;
+
+  sample_types = [
+    {val:"protein",label:"Protein"},
+    {val:'dna', label:'DNA'},
+    {val:'rna', label:'RNA'},
+    {val:'peptide', label:'Peptide'}];
+
   constructor(private globals_service: GlobalsService,
               public dialogRef: MdDialogRef<ReindexDialogComponent>,
               @Inject(MD_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+
     console.log(this.data);
+
+    this.model = {
+      'spacegroup': this.data.preferences.spacegroup,
+      'sample_type': this.data.preferences.sample_type
+    };
+
+    if (this.model.spacegroup === false) {
+      this.model.spacegroup = 0;
+    }
+
+    this.reindex_form = new FormGroup({
+      spacegroup: new FormControl(),
+      sample_type: new FormControl(),
+    });
+
+  }
+
+  submitReindex() {
+    console.log(this.reindex_form.value);
   }
 
 }
