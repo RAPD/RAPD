29c1b25d669e850b87241950ae4242f94a96a1c2
Frank Murphy <fvm3@cornell.edu>
2017-09-15 15:38:31 -0500

    Working on reintegrate form...

--- src/ui/src/app/plugin_components/mx/reintegrate-dialog/reintegrate-dialog.component.ts
+++ src/ui/src/app/plugin_components/mx/reintegrate-dialog/reintegrate-dialog.component.ts
@@ -44,13 +44,22 @@ export class ReintegrateDialogComponent implements OnInit {
       spacegroup: this.data.preferences.spacegroup,
       start_frame: 0, //this.data.preferences.spacegroup,
       end_frame: 360, //this.data.preferences.spacegroup,
-      // sample_type: this.data.preferences.sample_type,
       rounds_polishing: this.data.preferences.rounds_polishing,
       spacegroup_decider: this.data.preferences.spacegroup_decider,
+      low_res: this.data.preferences.low_res,
+      hi_res: this.data.preferences.hi_res,
     };
 
     if (this.model.spacegroup === false) {
       this.model.spacegroup = 0;
+    }
+
+    if (this.model.low_res === 0) {
+      this.model.low_res = 'None';
+    }
+
+    if (this.model.hi_res === 0) {
+      this.model.hi_res = 'None';
     }
 
     this.reintegrate_form = new FormGroup({
@@ -59,8 +68,47 @@ export class ReintegrateDialogComponent implements OnInit {
       end_frame: new FormControl(),
       sample_type: new FormControl(),
       rounds_polishing: new FormControl(),
-      spacegroup_decider: new FormControl()
+      spacegroup_decider: new FormControl(),
+      low_res: new FormControl(),
+      hi_res: new FormControl()
     });
   }
+
+  submitReintegrate() {
+
+    // Start to make the request object
+    let request: any = {};
+    // request.parent_result_id = this.data._id;
+    // request.image1_id = this.data.header1._id;
+    //
+    // // 2nd image?
+    // if (this.data.header2) {
+    //   request.image2_id = this.data.header2._id;
+    // } else {
+    //   request.image2_id = false;
+    // }
+
+    // Update the preferences with the form values
+    // request.preferences = Object.assign(this.data.preferences, this.reindex_form.value);
+
+    // Debugging
+    // console.log(this.reindex_form.value);
+
+    this.submitted = true;
+    // this.requests_service.submitRequest(request).subscribe(params => {
+    //   console.log(params);
+    //   this.submitted = false;
+    //   if (params.success === true) {
+    //     this.dialogRef.close();
+    //   }
+    //   // } else {
+    //   //   this.error_message = params;
+    //   // }
+    // });
+
+      // this.dialogRef.close(this.reindex_form.value);
+
+  }
+
 
 }
