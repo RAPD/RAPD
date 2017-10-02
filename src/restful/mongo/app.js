b916f4c05d765a8be1c41746bfbc8be84f4eb3c3
Frank Murphy <frankvmurphy@gmail.com>
2017-10-02 10:37:12 -0500

    Adding some logging

--- src/restful/mongo/app.js
+++ src/restful/mongo/app.js
@@ -283,9 +283,9 @@ apiRoutes.get('/', function(req, res) {
 // route middleware to verify a token
 apiRoutes.use(function(req, res, next) {
 
-  // console.log(req.body);
-  // console.log(req.query);
-  // console.log(req.headers);
+  console.log(req.body);
+  console.log(req.query);
+  console.log(req.headers);
 
   // check header or url parameters or post parameters for token
   var token = req.headers.authorization.replace('Bearer ', '');
