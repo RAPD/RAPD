b168497ffd5722391034c0b79677b0c088e60c16
Frank Murphy <frankvmurphy@gmail.com>
2017-10-03 09:39:53 -0500

    Making progress...

--- src/restful/mongo/app.js
+++ src/restful/mongo/app.js
@@ -249,6 +249,39 @@ apiRoutes.post('/authenticate', function(req, res) {
                       } else {
                         console.log('Group saved successfully', return_group);
 
+                        let m_user = {
+                          username:return_group.groupname,
+                          email:'',
+                          creator:'',
+                          timestamp:return_group.timestamp,
+                          status:return_group.status,
+                          role:'user',
+                          pass_force_change:false,
+                          pass_expire:false,
+                          loginAttempts:0,
+                          
+
+                        };
+
+                        // { _id: 59921becef080369c2f07cb2,
+                        //   username: 'Frank Murphy',
+                        //   group: null,
+                        //   email: 'fmurphy@anl.gov',
+                        //   __v: 3,
+                        //   creator: 59921becef080369c2f07cb2,
+                        //   timestamp: 2017-08-14T21:53:48.763Z,
+                        //   status: 'active',
+                        //   role: 'site_admin',
+                        //   pass_force_change: false,
+                        //   pass_expire: 2017-10-03T06:14:35.594Z,
+                        //   loginAttempts: 0,
+                        //   groups:
+                        //     [ { _id: 59921becef080369c2f07cb3, groupname: 'NECAT' },
+                        //       { _id: 59921becef080369c2f07cb3, groupname: 'NECAT' },
+                        //       { _id: 59921becef080369c2f07cb3, groupname: 'NECAT' },
+                        //       { _id: 59921becef080369c2f07cb3, groupname: 'NECAT' } ],
+                        //   created: 2017-09-28T18:37:05.470Z }
+
                         // // create a token
                         // var token = jwt.sign(user, app.get('superSecret'), {
                         //   expiresIn: 86400 // expires in 24 hours
