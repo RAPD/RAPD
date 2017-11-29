b91e2d8d74378ac17245f60612a27f3182c7a229
Frank Murphy <frankvmurphy@gmail.com>
2017-11-29 14:20:33 -0600

    Removing unused code

--- src/services/ldap/models/result.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var ResultSchema   = new Schema({}, {strict:false});
-
-module.exports = mongoose.model('Results', ResultSchema);
