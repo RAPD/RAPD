56ceed76cceff7a3ea9d5e8f20543306d6149b9a
Frank Murphy <fvm3@cornell.edu>
2017-12-01 16:35:26 -0600

    Add in localStorage for preferred image viewing color scheme

--- src/ui/src/app/plugin_components/mx/mx-image/mx-image.component.ts
+++ src/ui/src/app/plugin_components/mx/mx-image/mx-image.component.ts
@@ -18,53 +18,17 @@ export class MxImageComponent implements OnInit {
   @ViewChild('layer1') image_canvas: ElementRef;
   @ViewChild('layer2') drawing_canvas: ElementRef;
 
-  @Input()
-  result: any = {};
+  @Input() result: any = {};
+
   error_string: string;
   image_ctx: any;
 
-  view_color: string = 'Gray';  // Gray, Heat, Rainbow
+  view_color: string;  // Gray, Heat, Rainbow
 
   constructor(private rest_service: RestService) { }
 
   ngOnInit() {
-    // let ctx: CanvasRenderingContext2D =
-    // this.drawing_canvas.nativeElement.getContext('2d');
-    //
-    // // Draw the clip path that will mask everything else
-    // // that we'll draw later.
-    // ctx.beginPath();
-    // ctx.moveTo(250, 60);
-    // ctx.lineTo(63.8, 126.4);
-    // ctx.lineTo(92.2, 372.6);
-    // ctx.lineTo(250, 460);
-    // ctx.lineTo(407.8, 372.6);
-    // ctx.lineTo(436.2, 126.4);
-    // ctx.moveTo(250, 104.2);
-    // ctx.lineTo(133.6, 365.2);
-    // ctx.lineTo(177, 365.2);
-    // ctx.lineTo(200.4, 306.8);
-    // ctx.lineTo(299.2, 306.8);
-    // ctx.lineTo(325.2, 365.2);
-    // ctx.lineTo(362.6, 365.2);
-    // ctx.lineTo(250, 104.2);
-    // ctx.moveTo(304, 270.8);
-    // ctx.lineTo(216, 270.8);
-    // ctx.lineTo(250, 189);
-    // ctx.lineTo(284, 270.8);
-    // ctx.clip('evenodd');
-    //
-    // // Draw 50,000 circles at random points
-    // ctx.beginPath();
-    // ctx.fillStyle = '#DD0031';
-    // for (let i=0 ; i < 50000 ; i++) {
-    //   let x = Math.random() * 500;
-    //   let y = Math.random() * 500;
-    //   ctx.moveTo(x, y);
-    //   ctx.arc(x, y, 1, 0, Math.PI * 2);
-    // }
-    // ctx.fill();
-
+    this.view_color = localStorage.getItem('mx-image-view_color', 'Gray');
     this.readImage();
   }
 
@@ -72,6 +36,7 @@ export class MxImageComponent implements OnInit {
   doSomething(event:any) {
     console.log('doSomething');
     console.log(event);
+    localStorage.setItem('mx-image-view_color', this.view_color);
     this.readImage();
   }
 
