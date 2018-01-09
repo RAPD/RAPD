ef07aa6de549d4775705fbdf02f7ff5ca8f19c6a
Frank Murphy <fvm3@cornell.edu>
2018-01-09 15:14:32 -0600

    Fix websocket server to work with new version

--- src/services/rest/ws_server.js
+++ src/services/rest/ws_server.js
@@ -6,10 +6,10 @@ var http = require('http');
 var url = require('url');
 var WebSocketServer = require('ws').Server;
 // var SocketIo = require('socket.io');
-var mongoose = require('mongoose');
+var mongoose = require('./models/mongoose');
 Q = require('q');
 // Fix the promise issue in Mongoose
-mongoose.Promise = Q.Promise;
+// mongoose.Promise = Q.Promise;
 var Schema = mongoose.Schema;
 
 var jwt = require('jsonwebtoken');
@@ -373,15 +373,15 @@ function Wss (opt, callback) {
             // Get result details
             case 'get_result_details':
 
-              console.log('get_result_details', data.data_type, data.plugin_type);
+              console.log('get_result_details', data);
 
               // Create a mongoose model for the result
               let name = data.data_type+'_'+ data.plugin_type +'_result';
               let collection_name = name.charAt(0).toUpperCase() + name.slice(1);
               var ResultModel;
               try {
-                if (mongoose.model(collection_name)) {
-                  ResultModel = mongoose.model(collection_name);
+                if (mongoose.ctrl_conn.model(collection_name)) {
+                  ResultModel = mongoose.ctrl_conn.model(collection_name);
                 }
               } catch(e) {
                 if (e.name === 'MissingSchemaError') {
@@ -391,7 +391,8 @@ function Wss (opt, callback) {
                       auto: true
                       },
                     }, {strict:false});
-                  ResultModel = mongoose.model(collection_name, schema);
+                  // ResultModel = mongoose.model(collection_name, schema);
+                  ResultModel = mongoose.ctrl_conn.model(collection_name, schema);
                 }
               }
 
