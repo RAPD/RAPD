13dc13c4b043ea4bdfcb9d8fe444b9c2e2da64e6
Frank Murphy <fvm3@cornell.edu>
2018-02-26 12:36:06 -0600

    Update angular projects

--- src/ui/ng_update.sh
+++ src/ui/ng_update.sh
@@ -2,7 +2,7 @@
 
 npm uninstall -g @angular/cli
 npm cache clean --force
-npm install -g @angular/cli@1.7.0-rc.0
+npm install -g @angular/cli@6.0.0-beta.3
 rm -rf node_modules dist
-npm install --save-dev @angular/cli@1.7.0-rc.0
+npm install --save-dev @angular/cli@6.0.0-beta.3
 npm install
