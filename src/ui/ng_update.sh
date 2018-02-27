6f4622593fa4ae3e1c80f367cc16000dbcd0b437
Frank Murphy <fvm3@cornell.edu>
2018-02-27 14:23:20 -0600

    Downgrade angular-cli

--- src/ui/ng_update.sh
+++ src/ui/ng_update.sh
@@ -2,7 +2,7 @@
 
 npm uninstall -g @angular/cli
 npm cache clean --force
-npm install -g @angular/cli@6.0.0-beta.3
+npm install -g @angular/cli@1.6.8       
 rm -rf node_modules dist
-npm install --save-dev @angular/cli@6.0.0-beta.3
+npm install --save-dev @angular/cli@1.6.8       
 npm install
