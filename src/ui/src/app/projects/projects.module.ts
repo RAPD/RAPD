86590ee7bd007c1e550a31d6529186666a0713f5
Frank Murphy <fvm3@cornell.edu>
2018-03-13 16:58:58 -0500

    Playing with the project-mx

--- src/ui/src/app/projects/projects.module.ts
+++ src/ui/src/app/projects/projects.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RapdMaterialModule } from '../rapd.module';
+
 import { ProjectMxComponent } from './project-mx/project-mx.component';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
@@ -8,6 +10,7 @@ import { ProjectsRoutingModule } from './projects-routing.module';
 @NgModule({
   imports: [
     CommonModule,
+    RapdMaterialModule,
     ProjectsRoutingModule
   ],
   declarations: [
