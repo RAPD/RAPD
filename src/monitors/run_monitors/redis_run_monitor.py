d4ef6761fdef69f22b0ed8fb36a2a0ad8074af80
Jon Schuermann <schuerjp@anl.gov>
2018-04-06 16:11:06 -0500

    fixed run_monitor

--- src/monitors/run_monitors/redis_run_monitor.py
+++ src/monitors/run_monitors/redis_run_monitor.py
@@ -149,8 +149,6 @@ class Monitor(threading.Thread):
 
                 for run_list, site_tag in self.run_lists:
 
-                    self.logger.debug("Querying %s %s", run_list, site_tag)
-                    sys.exit()
                     # Try to pop the oldest image off the list
                     raw_run_data = self.redis.rpop(run_list)
 
