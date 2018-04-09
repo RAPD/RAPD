509d3ffd75e785cd640df69e19aee8aa2da45c36
Frank Murphy <frankvmurphy@gmail.com>
2018-04-09 16:33:30 -0500

    Add mongoose to jobs

--- src/services/rest/routes/jobs.js
+++ src/services/rest/routes/jobs.js
@@ -1,6 +1,6 @@
 var express  = require('express');
 var router   = express.Router();
-// var mongoose = require('../models/mongoose');
+var mongoose = require('../models/mongoose');
 const config = require('../config');
 
 // Redis
