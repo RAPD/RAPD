e1c6dcede573c1d2d0613a0fe9c51c760de48de7
Frank Murphy <frankvmurphy@gmail.com>
2019-04-13 10:12:23 -0500

    Update angular and material

--- src/ui/ng_update.sh
+++ src/ui/ng_update.sh
@@ -2,7 +2,7 @@
 
 npm uninstall -g @angular/cli
 npm cache verify
-npm install -g @angular/cli@7.3.7
+npm install -g @angular/cli@7.3.8
 rm -rf node_modules dist
-npm install --save-dev @angular/cli@7.3.7
+npm install --save-dev @angular/cli@7.3.8
 npm install
