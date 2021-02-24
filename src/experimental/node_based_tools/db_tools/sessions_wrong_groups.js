f2cfc718a879cadce96300ee9ab2688d89781bc2
Frank Murphy <fmurphy@anl.gov>
2021-02-24 11:26:28 -0600

    Code will corect most missing groups

--- src/experimental/node_based_tools/db_tools/sessions_wrong_groups.js
+++ src/experimental/node_based_tools/db_tools/sessions_wrong_groups.js
@@ -31,15 +31,36 @@ async function run() {
     while (await sessionsCursor.hasNext()) {
       counter += 1;
       const session = await sessionsCursor.next();
-      console.log(session);
+      if (session.group !== null) {
 
-      let group = await groupsCollection.findOne({_id:session.group});
-      console.log(group);
+        let group = await groupsCollection.findOne({_id:session.group});
 
-      if (group === null) {
-        console.log("!!!!!!!!!!");
-        sessionsCursor.close();
-      }
+        if (group === null) {
+          // console.log("!!!!!!!!!!");
+          // console.log(session);
+          // console.log(group);
+          let drd = session.data_root_dir; 
+          // console.log(drd);
+          if (drd) {
+
+            let splitDrd = drd.split("/");
+            let splitLen = splitDrd.length;
+            let shortInst = splitDrd[splitLen-2];
+            let shortName = splitDrd[splitLen-1].split("_")[0];
+
+            // console.log("shortInst", shortInst);
+            // console.log("shortName", shortName);
+
+            let newGroup = await groupsCollection.findOne({inst_short:shortInst,shortname:shortName});
+            // console.log(newGroup);
+            if (newGroup !== null) {
+              let updateResult = await sessionsCollection.updateOne({_id:session._id},{$set:{group:newGroup._id}});
+              console.log("Correcting", counter);
+            }
+            // sessionsCursor.close();
+          }
+        }
+    }
 
     //   await usersCollection.findOneAndUpdate({_id:user._id}, {$set:{groups:[user.group_id]}});
     }
